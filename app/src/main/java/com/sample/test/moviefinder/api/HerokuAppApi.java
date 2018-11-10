c93c1eb05b80531119752bcb0bf416781be7da31
aardines <aardines@securustechnologies.com>
2018-11-10 10:12:31 -0600

    Added retrofit interface

--- app/src/main/java/com/sample/test/moviefinder/api/HerokuAppApi.java
+++ app/src/main/java/com/sample/test/moviefinder/api/HerokuAppApi.java
@@ -1,8 +1,35 @@
 package com.sample.test.moviefinder.api;
 
+import com.sample.test.moviefinder.R;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by mac_tony on 11/10/18.
  */
 
 public class HerokuAppApi {
+    private final static String base_url_api = "https://movies-sample.herokuapp.com/api/movies";
+
+    public static Retrofit retrofit;
+
+    public HerokuAppApi() {
+    }
+
+    public static Retrofit getRetrofit(){
+        if(retrofit==null) {
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            OkHttpClient okHttpClient = builder.build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(base_url_api)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(okHttpClient)
+                    .build();
+        }
+        return retrofit;
+    }
 }
--- app/src/main/java/com/sample/test/moviefinder/api/HerokuAppInterface.java
+++ app/src/main/java/com/sample/test/moviefinder/api/HerokuAppInterface.java
@@ -1,8 +1,16 @@
 package com.sample.test.moviefinder.api;
 
+import com.sample.test.moviefinder.model.MovieResult;
+
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
 /**
  * Created by mac_tony on 11/10/18.
  */
 
 public interface HerokuAppInterface {
+    @GET()
+    Observable<MovieResult>getMovies();
 }
